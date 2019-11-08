0277c3bd962024bc3f2e10d17076fb36e1186f63
ZakirShaik <skz.zakir@gmail.com>
2019-11-08 00:33:02 -0600

    Feat: Add routing and footer

--- poke-app/src/app/app-routing.module.ts
+++ poke-app/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from './components/game/game.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'game',
+    component: GameComponent,
+    data: { title: 'Game'}
+  },
+  {
+    path: '',
+    redirectTo: '/game',
+    pathMatch: 'full'
+  },
+  {
+    path: '**', component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- poke-app/src/app/app.module.ts
+++ poke-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './components/game/game.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
